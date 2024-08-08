c0ec03bfeb3181df0bd1e1ece507271cb3e3777a
phukon <hello@rikiphukon.com>
2024-08-08 14:55:51 +0530

    feat: write transformed data to a file

--- checkCode.js
+++ checkCode.js
@@ -1,16 +1,33 @@
 const { ESLint } = require('eslint');
+const fs = require('fs');
+const path = require('path');
 
 (async function main() {
-  const eslint = new ESLint();
-  const results = await eslint.lintFiles(['samples/**/*.jsx']);
-  const transformedData = results.map(({ source, ...rest }) => rest);
+  try {
+    const eslint = new ESLint();
+    const results = await eslint.lintFiles(['samples/**/*.jsx']);
+    const transformedData = results.map(({ source, ...rest }) => rest);
+    const outputFilePath = path.join(__dirname, 'transformedData.json');
+    const transformedDataString = JSON.stringify(transformedData, null, 2);
 
-  console.log(`🎂 <<<start ${JSON.stringify(transformedData)} >>>end`);
+    if (results.some((result) => result.errorCount > 0)) {
+      console.error('ESLint errors found:');
+      console.log(`start>>>> ${transformedDataString} <<<<end`);
 
-  if (results.some((result) => result.errorCount > 0)) {
-    console.error('ESLint errors found:');
+      fs.writeFile(outputFilePath, transformedDataString, (err) => {
+        if (err) {
+          console.error('Error writing file:', err);
+          process.exit(1);
+        } else {
+          console.log(`Transformed data written to ${outputFilePath}`);
+          process.exit(1);
+        }
+      });
+    } else {
+      console.log('No ESLint errors found.');
+    }
+  } catch (error) {
+    console.error('Unexpected error:', error);
     process.exit(1);
-  } else {
-    console.log('No ESLint errors found.');
   }
 })();
